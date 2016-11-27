db9cbb52c416e9811eb8d08fbd42d0fb54e93f0d
Delyan Raychev <delyan@raychev.org>
2016-11-27 11:10:01 -0800

    Better comment

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -16,7 +16,7 @@ class Main {
     def getCells(s: String) = textPattern.findAllIn(s).map(removeAngles)
 
     def getUrl(url: String): Iterator[Iterator[String]] = {
-      // get the HTML source and remove new lines
+      // get the HTML source and remove new lines so regexes work correctly
       val html = Source.fromURL(url).mkString.filter(_ != '\n')
       rowPattern.findAllIn(html).map(getCells)
     }
