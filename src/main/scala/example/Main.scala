8815cccbc4b314266469cf34b2aba825f99e92e2
Delyan Raychev <delyan@raychev.org>
2016-11-27 11:04:37 -0800

    Get Yaesu Fusion Repeaters, parse, return

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,7 +1,26 @@
 package example
 
+import scala.io.Source
+
 class Main {
-  def getStockPrice(sym: String): Int = {
-    42
+  def getFusionStations(): Iterator[Iterator[String]] = {
+    val urls = Seq(
+      "http://fcs001.xreflector.net/mitte.html",
+      "http://fcs002.xreflector.net/mitte.html",
+      "http://laboenligne.ca/fcs003/mitte.html"
+    )
+
+    val rowPattern = "<tr>(.*?)</tr>".r
+    val textPattern = ">([a-zA-Z0-9 ]+)<".r
+    def removeAngles(s: String) = s.filter(_ != '<').filter(_ != '>')
+    def getCells(s: String) = textPattern.findAllIn(s).map(removeAngles)
+
+    def getUrl(url: String): Iterator[Iterator[String]] = {
+      // get the HTML source and remove new lines
+      val html = Source.fromURL(url).mkString.filter(_ != '\n')
+      rowPattern.findAllIn(html).map(getCells)
+    }
+
+    getUrl(urls(0))
   }
 }
